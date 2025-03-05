88a0c81099478249b8eb092c8bab8d1b8abb3a7a
Saul Barreto <saulbarretominaya@gmail.com>
2025-03-04 21:07:30 -0500

    Subiendo cambios de cryptomonedas 3.0

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Image, View, Text } from "react-native";
 import Header from './src/components/Header';
 import Formulario from './src/components/Formulario';
+import axios from 'axios';
 
 const App = () => {
+  const [moneda, guardarMoneda] = useState('');
+  const [criptomoneda, guardarCriptomoneda] = useState('');
+  const [consultarAPI, guardarConsultarAPI] = useState(false);
+
+  useEffect(() => {
+    const cotizarCriptomoneda = async () => {
+      if (consultarAPI) {
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+        const resultado = await axios.get(url);
+        console.log(resultado.data.DISPLAY[criptomoneda][moneda]);
+        guardarConsultarAPI(false);
+      }
+    }
+    cotizarCriptomoneda();
+  }, [consultarAPI]);
+
   return (
     <>
       <Header />
       <Image style={styles.imagen} source={require("./assets/img/cryptomonedas.png")} />
       <View style={styles.contenido}>
-        <Formulario />
+        <Formulario
+          moneda={moneda}
+          criptomoneda={criptomoneda}
+          guardarMoneda={guardarMoneda}
+          guardarCriptomoneda={guardarCriptomoneda}
+          guardarConsultarAPI={guardarConsultarAPI}
+        />
       </View>
     </>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,53 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, View, TouchableHighlight, Alert } from "react-native";
 import { Picker } from "@react-native-community/picker";
+import axios from "axios";
 
-const Formulario = () => {
-  const [moneda, guardarMoneda] = useState("");
-  const [criptomoneda, guardarCriptomoneda] = useState("");
+const Formulario = ({moneda,criptomoneda,guardarMoneda,guardarCriptomoneda,guardarConsultarAPI}) => {
+
+  const [criptomonedas, guardarCriptomonedas] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USM';
+      const resultado = await axios.get(url);
+      // guardarCriptomonedas(resultado.data.Data);
+      if (resultado.data && resultado.data.Data) {
+        guardarCriptomonedas(resultado.data.Data);
+      } else {
+        guardarCriptomonedas([]);
+      }
+    }
+
+    consultarAPI();
+  }, []);
+
   const obtenerMoneda = (moneda) => {
-    console.log(moneda);
+    guardarMoneda(moneda);
   }
+
+  const obtenerCriptomoneda = cripto => {
+    guardarCriptomoneda(cripto);
+  }
+
+  const CotizarPrecio = () => {
+    if(moneda.trim() === '' || criptomoneda.trim() === '') {
+      mostrarAlerta();
+      return;
+    }
+    guardarConsultarAPI(true);
+  }
+
+  const mostrarAlerta = () => {
+    Alert.alert(
+      'Error...',
+      'Ambos campos son obligatorios',
+      [
+        { text: 'OK' }
+      ]
+    )
+  }
+
   return (
     <View>
       <Text style={styles.label}>Moneda</Text>
@@ -24,6 +64,28 @@ const Formulario = () => {
       </Picker>
 
       <Text style={styles.label}> Criptomoneda </Text>
+
+      <Picker
+        selectedValue={criptomoneda}
+        onValueChange={cripto => obtenerCriptomoneda(cripto)}
+        itemStyle={{ height: 120 }}>
+        <Picker.Item label="-Seleccionar-" value="" />
+        {/* {criptomonedas.map(cripto => ( */}
+        {Array.isArray(criptomonedas) && criptomonedas.map(cripto => (
+          <Picker.Item
+            key={cripto.CoinInfo.Id}
+            label={cripto.CoinInfo.FullName}
+            value={cripto.CoinInfo.Name} />
+        ))}
+      </Picker>
+
+      <TouchableHighlight style={styles.btnCotizar}
+        onPress={() => CotizarPrecio()}>
+        <Text style={styles.textoCotizar}>Cotizar</Text>
+      </TouchableHighlight>
+
+      <Text> Cotizar </Text>
+
     </View>
   );
 };
@@ -34,6 +96,18 @@ const styles = StyleSheet.create({
     fontSize: 22,
     marginVertical: 20,
   },
+  btnCotizar: {
+    backgroundColor: "#5E49E2",
+    padding: 10,
+    marginTop: 20,
+  },
+  textoCotizar: {
+    color: "#FFF",
+    fontSize: 18,
+    textTransform: "uppercase",
+    textAlign: "center",
+  }
+
 });
 
 export default Formulario;
